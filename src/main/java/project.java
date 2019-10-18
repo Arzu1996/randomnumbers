84bd5ebd970b8d9f4255bbc0a555cdb46cf66cc8
Arzu1996 <49367330+Arzu1996@users.noreply.github.com>
2019-10-18 15:23:38 +0400

    Update project.java

--- src/main/java/project.java
+++ src/main/java/project.java
@@ -1,5 +1,47 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+
 public class project {
-    public static void main(String[] args) {
-        System.out.println(1);
+
+    public static void main(String[] args){
+
+
+
+        
+        System.out.println("Enter your name");
+ Scanner inputname = new Scanner(System.in); 
+ String name;
+ name = inputname.nextLine();
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        Scanner input = new Scanner(System.in);
+        int guess;
+        boolean win = false;
+ System.out.println("Let the game begin!");
+        while (win == false){  
+
+           
+            guess = input.nextInt();
+
+            if (guess == number){
+                win = true; 
+            }
+
+            else if(guess < number){
+                System.out.println("Your number is too small. Please, try again.");
+                
+            }
+
+            else if(guess > number){
+                System.out.println("Your number is too big. Please, try again.");
+                
+            }
+
+        }
+
+        System.out.println("Congratulations," + name + "!");
+
     }
 }
