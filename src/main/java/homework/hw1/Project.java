36a04caef990f9ce602ddfc1554e6749c8921fc0
Arzu1996 <harzu2996@gmail.com>
2019-10-21 10:10:15 +0400

    --correct mistakes

--- src/main/java/homework/hw1/Project.java
+++ src/main/java/homework/hw1/Project.java
@@ -4,41 +4,34 @@ import java.util.Random;
 import java.util.Scanner;
 
 
+public class Project {
 
-public class project {
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
 
 
-        
         System.out.println("Enter your name");
- Scanner inputname = new Scanner(System.in); 
- String name;
- name = inputname.nextLine();
+        Scanner inputName = new Scanner(System.in);
+        String name;
+        name = inputName.nextLine();
         Random rand = new Random();
         int number = rand.nextInt(100);
         Scanner input = new Scanner(System.in);
         int guess;
         boolean win = false;
- System.out.println("Let the game begin!");
-        while (win == false){  
+        System.out.println("Let the game begin!");
+        while (!win) {
 
-           
+
             guess = input.nextInt();
 
-            if (guess == number){
-                win = true; 
-            }
-
-            else if(guess < number){
+            if (guess == number) {
+                win = true;
+            } else if (guess < number) {
                 System.out.println("Your number is too small. Please, try again.");
-                
-            }
 
-            else if(guess > number){
+            } else {
                 System.out.println("Your number is too big. Please, try again.");
-                
+
             }
 
         }
