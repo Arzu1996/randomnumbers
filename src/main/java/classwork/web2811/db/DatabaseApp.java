3934bf97d4eb0a63c37b0ec757380c0e69038b82
Arzu1996 <yagut.badalova@gmail.com>
2019-11-28 14:01:54 +0400

    --classwork

---
+++ src/main/java/classwork/web2811/db/DatabaseApp.java
@@ -0,0 +1,14 @@
+package classwork.web2811.db;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class DatabaseApp {
+  public static void main(String[] args) throws SQLException {
+//    DbOps.insert_user("Mario", "mario@i.ua");
+    List<User> users = DbOps.get_all_users();
+    users.forEach(u ->
+        System.out.printf("%d : %-8s : %s\n",
+            u.getId(), u.getName(), u.getEmail()));
+  }
+}
